8942c4972bac9bc517d24699d56041bcb7c62178
albim72 <marcin.albiniak72@gmail.com>
2026-05-13 12:34:37 +0200

    Update MessageBus.cs

--- DZIEN_3/OKNO_24/MessageBus.cs
+++ DZIEN_3/OKNO_24/MessageBus.cs
@@ -1,24 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WpfDay3Exercise05;
-
-public static class MessageBus
+namespace TwoCommands
 {
-    private static readonly List<Action<RunnerSavedMessage>> RunnerSavedHandlers = new();
-
-    public static void Subscribe(Action<RunnerSavedMessage> handler)
+    internal class MessageBus
     {
-        RunnerSavedHandlers.Add(handler);
-    }
+        private static readonly List<Action<RunnerSavedMessage>> RunnerSavedHandlers = new();
 
-    public static void Send(RunnerSavedMessage message)
-    {
-        foreach (var handler in RunnerSavedHandlers.ToArray())
+        public static void Subscribe(Action<RunnerSavedMessage> handler)
         {
-            handler(message);
+            RunnerSavedHandlers.Add(handler);
+        }
+
+        public static void Send(RunnerSavedMessage message)
+        {
+            foreach (var handler in RunnerSavedHandlers.ToArray())
+            {
+                handler(message);
+            }
         }
     }
+    public record RunnerSavedMessage(string RunnerName);
 }
 
-public record RunnerSavedMessage(string RunnerName);
+// File: IsExternalInit.cs
+// Add this to your project to provide the missing type when targeting older frameworks/SDKs.
+namespace System.Runtime.CompilerServices
+{
+    // The compiler looks for this type to enable 'init' accessors and record support.
+    internal static class IsExternalInit { }
+}
